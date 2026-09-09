096ade4ce61e1cecd276d6d72436e3bb99abf35c
navetacandra <dev@navetacandraa.my.id>
2026-09-09 15:43:05 +0700

    i18n: set Layout lang to en

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -2,7 +2,7 @@ import { html } from 'hono/html';
 
 export const Layout = (props: { title: string; children: any }) => html`
 <!DOCTYPE html>
-<html lang="id">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
